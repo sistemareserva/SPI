80fae7c62da7b94df3c7c8bf4b7483b53e5644b2
sistemareserva <62656797+sistemareserva@users.noreply.github.com>
2020-04-16 13:33:58 -0400

    Version2

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+include('php/connection.php');
 // Ver en youtube session php para que entienda esta parte
 // https://www.youtube.com/watch?v=IAL6Nq6FW0s
 session_start();
@@ -39,9 +40,17 @@ esta ubicada en SPI/js/demo/chart-area-demo.js -->
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
 
   <!-- Custom styles for this template-->
   <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="alertifyJS/css/alertify.min.css">
+  <link rel="stylesheet" href="alertifyJS/css/themes/semantic.min.css">
+  <script src="alertifyJS/alertify.min.js"></script>
+
+  </script>
+
 
 </head>
 
@@ -58,7 +67,7 @@ esta ubicada en SPI/js/demo/chart-area-demo.js -->
         <div class="sidebar-brand-icon rotate-n-15">
           <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Bienvenido</div>
       </a>
 
       <!-- Divider -->
@@ -66,92 +75,11 @@ esta ubicada en SPI/js/demo/chart-area-demo.js -->
 
       <!-- Nav Item - Dashboard -->
       <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="index.php">
           <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
+          <span>Home</span></a>
       </li>
 
-      <!-- Divider -->
-      <hr class="sidebar-divider">
-
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Interface
-      </div>
-
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-cog"></i>
-          <span>Components</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.html">Buttons</a>
-            <a class="collapse-item" href="cards.html">Cards</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities" aria-expanded="true" aria-controls="collapseUtilities">
-          <i class="fas fa-fw fa-wrench"></i>
-          <span>Utilities</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.html">Colors</a>
-            <a class="collapse-item" href="utilities-border.html">Borders</a>
-            <a class="collapse-item" href="utilities-animation.html">Animations</a>
-            <a class="collapse-item" href="utilities-other.html">Other</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider">
-
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Addons
-      </div>
-
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-          <i class="fas fa-fw fa-folder"></i>
-          <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Login Screens:</h6>
-            <a class="collapse-item" href="login.html">Login</a>
-            <a class="collapse-item" href="register.html">Register</a>
-            <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-            <div class="collapse-divider"></div>
-            <h6 class="collapse-header">Other Pages:</h6>
-            <a class="collapse-item" href="404.html">404 Page</a>
-            <a class="collapse-item" href="blank.html">Blank Page</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Nav Item - Charts -->
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-      </li>
-
-      <!-- Nav Item - Tables -->
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
-      </li>
 
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
@@ -366,11 +294,13 @@ esta ubicada en SPI/js/demo/chart-area-demo.js -->
           <div class="row">
 
             <!-- Area Chart -->
-            <div class="col-xl-12 col-lg-7">
+            <div class="col-lg-6 mb-4">
               <div class="card shadow mb-4">
                 <!-- Card Header - Dropdown -->
                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
                   <h6 class="m-0 font-weight-bold text-primary">Reservar Implementos</h6>
+
+
                   <button name="" id="Edit" type="button"  onclick='actualizar()' class="btn btn-primary">Actualizar</button>
 
                   <!-- <div class="dropdown no-arrow">
@@ -385,6 +315,9 @@ esta ubicada en SPI/js/demo/chart-area-demo.js -->
                       <a class="dropdown-item" href="#">Something else here</a>
                     </div>
                   </div> -->
+                </div>
+                <div id ="alert"class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
                 </div>
                 <!-- Card Body -->
                 <div class="card-body">
@@ -406,7 +339,8 @@ esta ubicada en SPI/js/demo/chart-area-demo.js -->
 
                     <div class="chart-area ">
 
-                    <table  class= " table table-fixed ">
+
+                    <table id="tabResImp" class= " table table-fixed ">
 
                       <thead >
                       <tr >
@@ -417,11 +351,26 @@ esta ubicada en SPI/js/demo/chart-area-demo.js -->
 
                       </tr>
                       </thead>
-                      <tbody id='elemento' >
+                      <!-- <div id="alert">
+                        <div class="modal-dialog alert alert-danger" role="alert">
+                          <div class="modal content">
+                            <div class="modal-header">
+                              <button type="button" class="close" data-dimiss="modal" aria-label="Close">
+                               &times;
+                              </button>
+                                Usted tiene  <h7 class="alert-link">prestado o reservado</h7> un implemento.
+                            </div>
+                          </div>
+                        </div>
+                      </div> -->
+
+                       <tbody id='elemento' >
 
 
-                      </tbody>
-                    </table>
+                       </tbody>
+                         </table>
+
+
 
                     <!-- <canvas id="myAreaChart"></canvas> -->
                   </div>
@@ -430,7 +379,67 @@ esta ubicada en SPI/js/demo/chart-area-demo.js -->
             </div>
 
             <!-- Pie Chart -->
+            <div class="col-lg-6 mb-4">
+              <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                  <h6 class="m-0 font-weight-bold text-primary">Reservaciones</h6>
 
+
+
+
+                  <!-- <div class="dropdown no-arrow">
+                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+                      <div class="dropdown-header">Dropdown Header:</div>
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <div class="dropdown-divider"></div>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div> -->
+                </div>
+                <!-- Card Body -->
+                <div class="card-body">
+
+
+                    <div class="input-group mb-3">
+
+
+                  </div>
+
+
+                    <div class="chart-area ">
+
+
+                    <table id="ReseradoImp" class= " table table-fixed ">
+
+                      <thead >
+                      <tr >
+                          <th>Referencia</th>
+                          <th>Nombre</th>
+                          <th>Hora de Reserva</th>
+                          <th>  </th>
+
+                      </tr>
+                      </thead>
+
+
+                       <tbody id='implreservado' >
+
+
+                       </tbody>
+                         </table>
+
+
+
+                    <!-- <canvas id="myAreaChart"></canvas> -->
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
           <!-- Content Row -->
